a26a856a82ef175d6e47ea800d121120b8448356
dtindy <dy83421@gmail.com>
2021-06-09 02:09:17 +0530

    update menu-items withRouter and add link to /shop/hats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './App.css';
+import { Route } from 'react-router-dom';
 import HomePage from "./pages/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+); 
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
