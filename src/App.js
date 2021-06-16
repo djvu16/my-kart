8db96a028d8dc79215bec46e6a0171b775c50e51
dtindy <dy83421@gmail.com>
2021-06-16 23:26:34 +0530

    signin using email and password

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
               ...snapShot.data()
             }
             });
-            console.log(this.state);
+            
         });
         
       }
--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -7,7 +7,7 @@ import "./signin.styles.scss";
 //components
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {signInWithGoogle} from "../../firebase/firebase.utils";
+import { auth,signInWithGoogle} from "../../firebase/firebase.utils";
 
 
 class SignIn extends Component{
@@ -26,9 +26,18 @@ class SignIn extends Component{
         const { value,name}=event.target;
         this.setState({[name]:value});
     }
-    handleSubmit=(event)=>{
+    handleSubmit= async (event)=>{
         event.preventDefault();
-        this.setState({email:'',password:""});
+        const { email,password } =this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:""});
+        } catch (error) {
+            console.log(error);
+        }
+
+        
     }
 
     render(){
